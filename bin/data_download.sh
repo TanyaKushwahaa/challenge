c194b417578d8d1de65651c34a54dee0de89d9c5
TanyaKushwahaa <KushwahaaT@cardiff.ac.uk>
2023-03-29 11:23:27 +0100

    automated data retreival

---
+++ bin/data_download.sh
@@ -0,0 +1,4 @@
+curl -L -o data/survey_automate.csv https://energydata.info/dataset/a27a9b60-706b-4c81-8608-c913d2ed998f/resource/fdef4f22-fe57-49b1-9c42-e5dac79cc90c/download/pakistanbiomassfieldsurvey.csv
+
+
+
